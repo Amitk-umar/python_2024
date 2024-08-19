9b3674857248bc69c62b2f558c79b1082a04761e
Amitk-umar <135205505+Amitk-umar@users.noreply.github.com>
2024-08-19 14:13:35 +0530

    list_method

---
+++ 01_list.py
@@ -0,0 +1,8 @@
+friends = ["Apple","Orange",5,250.66,False,"Raghav","chopra"]
+
+print(friends[0])
+friends[1]="Watermelon" #Unlike string list are mutable
+print(friends[1]) 
+
+print(friends[0:3])
+print(friends[0:3:2])
---
+++ 02_list_method.py
@@ -0,0 +1,17 @@
+list_method = ["Akash","Rahul",25,220.658,False,"Pineapple","Doraemon"]
+# print(list_method)
+
+list_method.append("Machine Learning")
+# print(list_method)
+
+l1 = [1,2,54,36,85,2,2.032]
+# l1.sort()
+# l1.reverse()
+# l1.insert(4,"rakesh")
+new_value_pop = l1.pop(4)
+# print(new_value_pop)
+# print(l1)
+
+
+print(l1.remove(36))  # This will show the updated list without the value 36
+print(l1)
